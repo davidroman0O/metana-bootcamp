257e1072025db7a9fec7944f6cd2109dfea5a0ea
David Roman <romandavid83@gmail.com>
2025-01-31 09:16:00 -0500

    continue

---
+++ module-1/00-smartcontracts/010 - Constants/Constants.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Constants {
+    address public constant MY_ADDRESS = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+    uint public constant MY_UINT = 123;
+}
+
+contract Var {
+    address public MY_ADDRESS = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+}
