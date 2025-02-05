ec02e6486efb8830ac8d6dfea82b399f331f9087
David Roman <romandavid83@gmail.com>
2025-02-05 16:31:16 -0500

    Update ERCTokenSale.sol

--- module-1/01-assignments/ERCTokenSale.sol
+++ module-1/01-assignments/ERCTokenSale.sol
@@ -8,7 +8,8 @@ import "@openzeppelin/contracts/access/Ownable2Step.sol";
 // I'm not sure if i understood that decimal thing with that contract
 contract ERCTokenSale is ERC20, Ownable2Step {
 
-    uint256 constant public MAX_SUPPLY = 1_000_000; // 1M tokens max
+    uint256 constant public MAX_SUPPLY = 1_000_000 * 1e18; // scale to 18 decimal
+    uint256 constant public TOKEN_PER_ETH = 1000 * 1e18; // 18 decimals too
 
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) Ownable(msg.sender) {
         _transferOwnership(msg.sender);
@@ -18,12 +19,15 @@ contract ERCTokenSale is ERC20, Ownable2Step {
         revert("You can't send ether with data on that contract");
     }
 
+    event Mint(address indexed sender, uint256 ethSent, uint256 tokensMinted, uint256 totalSupplyBefore);
+
     function mintTokens() public payable {
-        require(msg.value % 1 ether == 0, "send a multiple of 1 ETH");
-        require(totalSupply() <= MAX_SUPPLY, "cannot mint more tokens");
-        uint256 minter = 1000 * (msg.value / 1 ether);
-        require(totalSupply() + minter <= MAX_SUPPLY, "cannot mint more excess tokens");
-        super._mint(msg.sender, 1000 * (msg.value / 1 ether));
+         // require(msg.value % 1 ether == 0, "send a multiple of 1 ETH");
+        require(msg.value > 0, "Must send ETH to mint tokens"); // whatever amount
+        uint256 tokensToMint = (TOKEN_PER_ETH * msg.value) / 1 ether;
+        require(totalSupply() + tokensToMint <= MAX_SUPPLY, "Purchase would exceed max token supply");
+        emit Mint(msg.sender, msg.value, tokensToMint, totalSupply());
+        super._mint(msg.sender, tokensToMint);
     }
 
     function withdraw() external onlyOwner {
