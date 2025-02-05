e1f90ea6481eebc628e66ef0b860b1921b36a1e6
David Roman <romandavid83@gmail.com>
2025-02-05 09:11:56 -0500

    Create FacesNFT.sol

---
+++ module-2/01-assignments/FacesNFT.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract FacesNFT is ERC721 {
+
+    uint256 constant MAX_SUPPLY = 10;
+
+    constructor() ERC721("Echoforms", "ECHO") {
+        for (uint16 i = 1; i < 11; i++) {
+            super._safeMint(msg.sender, i);
+        }
+    }
+
+    fallback() external payable {
+        revert("You can't send ether with data on that contract");
+    }
+
+    receive() external payable {
+        revert("The NFT is free, no need to send ETH");
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://bafybeia3kfwpeqqxpwimflmqclo3hwewa7ziueennq2wocj3nlvv34bq34/";
+    }
+}
+ 
