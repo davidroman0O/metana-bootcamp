9eef79c611c96eac5ae523520421db27ed256abd
David Roman <romandavid83@gmail.com>
2025-02-09 22:52:34 -0500

    Update 01-SimpleNFT.sol

--- module-2/01-assignments/01-SimpleNFT.sol
+++ module-2/01-assignments/01-SimpleNFT.sol
@@ -3,9 +3,6 @@ pragma solidity ^0.8.22;
 
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// Online already https://opensea.io/collection/echoforms
-// Verified https://polygonscan.com/address/0xb666b27e3a7cffd89a49d820ed65d94cdd1f428c#code
-// https://polygonscan.com/verifyContract-solc-json?a=0xb666b27e3a7cffd89a49d820ed65d94cdd1f428c&c=v0.8.26%2bcommit.8a97fa7a&lictype=3
 contract FacesNFT is ERC721 {
 
     constructor() ERC721("Echoforms", "ECHO") {
